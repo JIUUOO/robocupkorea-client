7c28c8c1171c9b7165971fca92371e0d3bcc8384
JIUUOO <nninjiuuoo@gmail.com>
2025-01-17 20:08:46 +0900

    💄 Design: 홈 페이지에 공지 컴포넌트 제거

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -65,7 +65,7 @@ export default function Home() {
         </CardGroup>
       </CardContainer>
 
-      <CardContainer>
+      {/* <CardContainer>
         <CardGroup>
           <Card
             title="Notices"
@@ -76,7 +76,7 @@ export default function Home() {
             varient="introduce"
           />
         </CardGroup>
-      </CardContainer>
+      </CardContainer> */}
 
       {/* <CardContainer>
         <CardGroup>
