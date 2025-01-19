f4c5ef06bcf8f7bda70893db444948d4ba4e831e
JIUUOO <nninjiuuoo@gmail.com>
2025-01-19 12:03:03 +0900

    ✅ Test: 모바일 환경 텍스트 고정, 비디오 재생 테스트

--- src/components/home/landing/LadingInfoDefault.tsx
+++ src/components/home/landing/LadingInfoDefault.tsx
@@ -1,6 +1,6 @@
 export default function LandingInfoDefault() {
   return (
-    <div className="fixed h-full w-full min-w-[376px]">
+    <div className="pb-safe-bottom fixed h-full w-full min-w-[376px]">
       <div className="container mt-16 flex h-full w-full flex-col items-center justify-center md:mt-24">
         <div className="flex flex-col gap-2 text-center text-white">
           <div className="text-shadow text-4xl font-bold md:text-6xl lg:text-7xl xl:text-8xl">
--- src/components/home/landing/LandingVideo.tsx
+++ src/components/home/landing/LandingVideo.tsx
@@ -3,7 +3,7 @@ import Landing from "@/assets/videos/landing.mp4";
 export default function LandingVideo() {
   return (
     <div className="pointer-events-none fixed flex w-full justify-center">
-      <video className="h-screen w-full object-cover" autoPlay loop muted preload="metadata">
+      <video className="h-screen w-full object-cover" autoPlay loop muted preload="metadata" playsInline>
         <source src={Landing} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
