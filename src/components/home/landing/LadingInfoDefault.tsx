476e7a035fd287bf59b81ea3d5b04347250306c0
JIUUOO <nninjiuuoo@gmail.com>
2025-01-19 20:33:57 +0900

    ✅ Test: 폰트, 랜딩 비디오, 텍스트 테스트

--- src/components/home/landing/LadingInfoDefault.tsx
+++ src/components/home/landing/LadingInfoDefault.tsx
@@ -1,6 +1,6 @@
 export default function LandingInfoDefault() {
   return (
-    <div className="fixed left-0 top-0 h-dvh w-screen min-w-[376px]">
+    <div className="fixed left-0 top-0 h-svh w-screen min-w-[376px]">
       <div className="container mt-16 flex h-full w-full flex-col items-center justify-center md:mt-24">
         <div className="flex flex-col gap-2 text-center text-white">
           <div className="text-shadow text-nowrap text-4xl font-bold md:text-6xl lg:text-7xl xl:text-8xl">
