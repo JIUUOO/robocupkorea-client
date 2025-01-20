fca5ed5eaa5b2d704f052696b2616043752b88ec
JIUUOO <nninjiuuoo@gmail.com>
2025-01-20 10:26:45 +0900

    💄 Design: 텍스트 조정, 언론 표시

--- src/data/about/newsData.ts
+++ src/data/about/newsData.ts
@@ -3,12 +3,14 @@ export const newsData = [
     title: "제5회 한국창의코딩대회 15일 성료",
     subtitle: "6개 종목 284명 참가...자율주행 챌린지 U19 종목 쿨팀 우승",
     date: "2024-08-19",
+    press: "로봇신문",
     reference: "http://www.irobotnews.com/news/articleView.html?idxno=35817",
   },
   {
     title: "2024 코리아 로보컵 오픈대회 18일 성료",
     subtitle: "4개국 500여명 참가해 열띤 경쟁 벌여...휴머노이드 축구 키즈 국민대 쿠도스 12기 팀 승리",
     date: "2024-02-19",
+    press: "로봇신문",
     reference: "http://www.irobotnews.com/news/articleView.html?idxno=34077",
   },
 ];
