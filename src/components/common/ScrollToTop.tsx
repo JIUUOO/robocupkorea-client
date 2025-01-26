330f7081737cc059564b16f6394899fdabd7d05a
JIUUOO <nninjiuuoo@gmail.com>
2025-01-26 21:01:36 +0900

    🐛 Fix: lenis 추가로 인해 관성때문에 스크롤 초기화가 되지 않는 문제 해결

--- src/components/common/ScrollToTop.tsx
+++ src/components/common/ScrollToTop.tsx
@@ -1,16 +1,18 @@
+import { useLenis } from "lenis/react";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
   const { pathname, hash } = useLocation();
+  const lenis = useLenis();
 
   useEffect(() => {
-    if (!hash) {
-      setTimeout(() => {
-        window.scrollTo(0, 0);
-      }, 0);
+    if (lenis && !hash) {
+      lenis.scrollTo(0, {
+        immediate: true, // 관성 없이 즉시 초기화
+      });
     }
-  }, [pathname, hash]);
+  }, [pathname, hash, lenis]);
 
   return null;
 }
